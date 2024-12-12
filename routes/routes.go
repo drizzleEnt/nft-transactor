ac966b21548ef20c7946cce1a9186dbe82a279d3
skit <drizzleent@yandex.ru>
2024-12-12 20:25:59 +0300

    service layer

--- routes/routes.go
+++ routes/routes.go
@@ -12,16 +12,28 @@ func SetupRouter(db *sql.DB) *http.ServeMux {
 
 	// TOKENS
 	r.HandleFunc("/tokens/create", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+
+			return
+		}
 		tokenController := controller.NewTokenController()
 		tokenController.CreateToken(w, r)
 	})
 
 	r.HandleFunc("/tokens/list", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+
+			return
+		}
 		tokenController := controller.NewTokenController()
 		tokenController.CreateToken(w, r)
 	})
 
 	r.HandleFunc("/tokens/total_supply", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+
+			return
+		}
 		tokenController := controller.NewTokenController()
 		tokenController.CreateToken(w, r)
 	})
--- service/service.go
+++ service/service.go
@@ -1,1 +1,23 @@
 package service
+
+type TokenService interface {
+}
+
+type tokenService struct {
+}
+
+func NewTokenService() TokenService {
+	return &tokenService{}
+}
+
+func (s *tokenService) CreateToken() {
+
+}
+
+func (s *tokenService) ListToken() {
+
+}
+
+func (s *tokenService) TotalSupplyToken() {
+
+}
